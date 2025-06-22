bb0e3a2331f0d274426b22a6e3dffb1eaf7918a5
Rakesh <rakesh2003cs@gmail.com>
2025-06-23 01:44:08 +0530

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { motion } from "framer-motion";
-import resume from '../../assets/images/resume.pdf';
+import resume from '../../../assets/images/resume.pdf';
 
 const About = () => {
   return (
