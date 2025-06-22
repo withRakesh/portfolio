4cd130826ecb0b747778ddc23a0bfb03ef6e2182
Rakesh <rakesh2003cs@gmail.com>
2025-06-23 01:04:10 +0530

    Update TechStack.jsx

--- src/components/techstack/TechStack.jsx
+++ src/components/techstack/TechStack.jsx
@@ -1,7 +1,7 @@
  import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { motion } from "framer-motion";
-import htmllogo from '../src/assets/images/html.png'
+import htmllogo from '/src/assets/images/html.png'
  
 
  
