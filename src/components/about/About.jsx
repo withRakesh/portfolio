3153bdaa860e3b4fe60a9c5c48efb32945ba26dc
withRakesh <rakesh2003cs@gmail.com>
2025-06-23 12:37:43 +0530

    updated

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { motion } from "framer-motion";
-import resume from '../../../assets/images/resume.pdf';
+ 
 
 const About = () => {
   return (
@@ -24,7 +24,7 @@ const About = () => {
                 creating scalable, responsive, and user-friendly solutions using
                 MongoDB, Express.js, React.js, and Node.js.
               </p>
-              <a href={resume} target="_blank" rel="noopener noreferrer"  download>
+              <a href="" target="_blank" rel="noopener noreferrer"  download>
                 <button  className="mt-3 px-4 py-2 fw-semibold">
                   Download CV
                 </button>
--- src/components/techstack/TechStack.jsx
+++ src/components/techstack/TechStack.jsx
@@ -1,69 +1,59 @@
- import React from "react";
+import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { motion } from "framer-motion";
-import html from '/src/assets/images/html.png'
- import css from '/src/assets/images/css3.png'
-import javascript from '/src/assets/images/javascript.png'
-import bootstrap from '/src/assets/images/bootstrap.png'
-import react from '/src/assets/images/react.png'
-import framer from '/src/assets/images/framer.png'
-import node from '/src/assets/images/node.png'
-import express from '/src/assets/images/express.png'
-import mongo from '/src/assets/images/mongodb.png'
-import git from '/src/assets/images/git.png'
-import github from '/src/assets/images/github.png'
 
- 
 const TechStack = () => {
-
-    const techLogos = [
-  { name: "HTML5", src: html },
-  { name: "CSS3", src: css },
-  { name: "JavaScript", src:javascript  },
-  { name: "Bootstrap", src: bootstrap  }, 
-  { name: "React", src: react}, 
-    { name: "Framer motion", src: framer }, 
-    { name: "Node js", src: node },  
-  { name: "Express js", src: express  },   
-  { name: "Mongo db", src: mongo },       
-  { name: "Git", src: git },
-    { name: "GitHub", src:github },
-       
-   
-];
+  const techLogos = [
+    { name: "HTML5", src: "/src/assets/images/html.png" },
+    { name: "CSS3", src: "/src/assets/images/css3.png" },
+    { name: "JavaScript", src: "/src/assets/images/javascript.png" },
+    { name: "React", src: "/src/assets/images/react.png" },
+    { name: "Bootstrap", src: "/src/assets/images/bootstrap.png" },
+    { name: "Framer Motion", src: "/src/assets/images/framer.png" },
+    { name: "Node js", src: "/src/assets/images/node.png" },
+    { name: "Express js", src: "/src/assets/images/express.png" },
+    { name: "Mongo db", src: "/src/assets/images/mongodb.png" },
+    { name: "Git", src: "/src/assets/images/git.png" },
+    { name: "GitHub", src: "/src/assets/images/github.png" },
+  ];
 
   return (
-    <motion.div initial={{opacity:0, scale:0.8}}
-    whileInView={{opacity:1, scale:1}}
-    transition={{duration:0.8}}
-    viewport={{once:true, amount:0.3}}
+    <motion.div
+      initial={{ opacity: 0, scale: 0.8 }}
+      whileInView={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.8 }}
+      viewport={{ once: true, amount: 0.3 }}
     >
       <section className="py-5 text-center">
-      <Container >
-        <h2 className="fw-bold text-dark mb-2">My Tech Stack</h2>
-        <p className="text-muted mb-5">Technologies I’ve been working with recently</p>
-        <Row className="justify-content-center g-4">
-          {techLogos.map((tech, idx) => (
-            <Col key={idx} xs={4} sm={3} md={2} lg={3} >
-              <img
-              className="text-center mb-2"
-                src={tech.src}
-                alt={tech.name}
-                title={tech.name}
-                style={{ width: "50px", height: "50px", objectFit: "contain" }}
-              />
-              <h6>{tech.name}</h6>
-        
-            </Col>
-          ))}
+        <Container>
+          <h2 className="fw-bold text-dark mb-2">My Tech Stack</h2>
+          <p className="text-muted mb-5">
+            Technologies I’ve been working with recently
+          </p>
+          <Row className="justify-content-center g-4">
+            {techLogos.map((tech, idx) => (
+              <Col key={idx} xs={4} sm={3} md={2} lg={3}>
+                <img
+                  className="text-center mb-2"
+                  src={tech.src}
+                  alt={tech.name}
+                  title={tech.name}
+                  style={{
+                    width: "50px",
+                    height: "50px",
+                    objectFit: "contain",
+                  }}
+                />
+                <h6>{tech.name}</h6>
+              </Col>
+            ))}
 
-          <col />
-        </Row>
-        <hr />
-      </Container>
-    </section>
+            <col />
+          </Row>
+          <hr />
+        </Container>
+      </section>
     </motion.div>
-     
   );
 };
 
